6362ec037e445d605b238cc10dfec1ab6d4f37f2
aviharez <elzainsyifa@gmail.com>
2020-06-15 13:48:09 +0700

    fixing error

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object) :
-    HOST = str(os.environ.get("DB_HOST"))
-    DATABASE = str(os.environ.get("DB_DATABASE"))
-    USERNAME = str(os.environ.get("DB_USERNAME"))
-    PASSWORD = str(os.environ.get("DB_PASSWORD"))
+    HOST = "remotemysql.com"
+    DATABASE = "CIvte6ww4R"
+    USERNAME = "CIvte6ww4R"
+    PASSWORD = "qZLXwzRDvO"
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://' + USERNAME + ':' + PASSWORD + '@' + HOST + '/' + DATABASE
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_RECORD_QUERIES = True

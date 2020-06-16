9c8b4f2306a7a9a2a5506a7c4e13f4b2c64b4976
aviharez <elzainsyifa@gmail.com>
2020-06-16 14:17:43 +0700

    change db

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object) :
-    HOST = "remotemysql.com"
-    DATABASE = "CIvte6ww4R"
-    USERNAME = "CIvte6ww4R"
-    PASSWORD = "qZLXwzRDvO"
+    HOST = "us-cdbr-east-05.cleardb.net"
+    DATABASE = "heroku_bdbde6549454283"
+    USERNAME = "bf09fdcd61c909"
+    PASSWORD = "85a775f5"
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://' + USERNAME + ':' + PASSWORD + '@' + HOST + '/' + DATABASE
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_RECORD_QUERIES = True
